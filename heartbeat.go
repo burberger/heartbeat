bb63cfd3f72a5102544172369e50b1e96965adfb
Bob Urberger <burberger@gmail.com>
2014-01-30 00:38:27 -0800

    Commented my code like a professional

--- heartbeat.go
+++ heartbeat.go
@@ -13,22 +13,28 @@ import (
 	"time"
 )
 
+// Node holds the information representing a heartbeat client
 type Node struct {
 	Hostname  string
 	Timestamp time.Time
 }
 
+// Declare global node map and mutex for cross thread access
 var (
 	m          sync.RWMutex
 	live_hosts map[string]Node
 )
 
+// Precompile template
 var templates = template.Must(template.ParseFiles("list.html"));
 
+// When you recieve a new connection from a heartbeat client,
+// Lock the table for writing and build / update a node.
 func connection_handler(conn net.Conn) {
 	dec := gob.NewDecoder(conn)
 	var heartbeat Node
 	dec.Decode(&heartbeat)
+    // Strip transmit port number from connection
 	addr := conn.RemoteAddr().String()
 	addr = strings.Split(addr, ":")[0]
 	m.Lock()
@@ -36,6 +42,8 @@ func connection_handler(conn net.Conn) {
 	m.Unlock()
 }
 
+// Processes the map for dead hosts and logs who the system is aware of.
+// Read lock allows web requests to come in during a map check cycle.
 func map_check(sleepTime time.Duration) {
 	for {
 		m.RLock()
@@ -51,6 +59,7 @@ func map_check(sleepTime time.Duration) {
 	}
 }
 
+// Dispatches connections to goroutines for heartbeat server
 func server(port string) {
 	ln, err := net.Listen("tcp", ":"+port)
 	if err != nil {
@@ -66,13 +75,17 @@ func server(port string) {
 	}
 }
 
+// Responds to web server requests for map data
 func root_handler(w http.ResponseWriter, r *http.Request) {
+    m.RLock()
     err := templates.ExecuteTemplate(w, "list.html", live_hosts)
+    m.RUnlock()
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
     }
 }
 
+// Open a connection to the server, attempt to beacon, close connection
 func client(ip, port string, sleepTime time.Duration) {
 	for {
 		conn, err := net.Dial("tcp", ip+":"+port)
@@ -92,10 +105,13 @@ func client(ip, port string, sleepTime time.Duration) {
 }
 
 func main() {
+    // Command line flags
 	ip := flag.String("client", "", "Starts program in client mode.  Requires address of server.")
 	sleepTime := flag.Duration("t", time.Minute*10, "Expected beacon delay.  Affects both client and server modes.")
 	port := flag.String("port", "5656", "Specify the port server is running on.")
 	flag.Parse()
+
+    // No client flag, serve stuff
 	if *ip == "" {
 		live_hosts = make(map[string]Node)
 		go map_check(*sleepTime)
